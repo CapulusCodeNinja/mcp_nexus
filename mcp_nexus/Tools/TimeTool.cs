3a5770832c898630db5923ffdf15e57b47bd643d
CapulusCodeNinja <david.roller@gendigital.com>
2025-09-18 18:43:01 +0200

    Project structure and first tools

---
+++ mcp_nexus/Tools/TimeTool.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using ModelContextProtocol.Server;
+
+namespace mcp_nexus.Tools
+{
+    [McpServerToolType]
+    public static class TimeTool
+    {
+        [McpServerTool, Description("Gets the current time for a city")]
+        public static string GetCurrentTime(string city) =>
+            $"It is {DateTime.Now.Hour}:{DateTime.Now.Minute} in {city}.";
+    }
+}
---
+++ mcp_nexus/Tools/WindbgTool.cs
@@ -0,0 +1,13 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace mcp_nexus.Tools
+{
+    [McpServerToolType]
+    public class WindbgTool
+    {
+        [McpServerTool, Description("Gets the current time for a city")]
+        public static string GetCurrentTime(string city) =>
+            $"It is {DateTime.Now.Hour}:{DateTime.Now.Minute} in {city}.";
+    }
+}
